7de0ba102492f3dff762096d045c4baf39462795
Krzysztof Poznakowski <kpoznak@gmail.com>
2025-04-07 17:11:04 +0200

    React wip

--- blocks/testcomponent/testcomponent.js
+++ blocks/testcomponent/testcomponent.js
@@ -1,15 +1,17 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable import/no-self-import */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable import/extensions */
+import ReactDOM from 'react-dom/client';
+import MyReactComponent from './testcomponent';
+
 export default function decorate(block) {
-    console.log('Block:', block);
-    console.log('Block dataset:', block.dataset);
-    console.log('Inner HTML:', block.innerHTML);
+//   const props = {
+//     message: '',
+//   };
 
-    const title = block.dataset.title || 'Default Title';
-    const subtitle = block.dataset.subtitle || 'Default Subtitle';
+  block.innerHTML = ''; // Clear existing content
 
-    block.innerHTML = `
-        <div class="testcomponent-content">
-            <h1>${title}</h1>
-            <p>${subtitle}</p>
-        </div>
-    `;
+  const root = ReactDOM.createRoot(block);
+  root.render(<MyReactComponent message={'Testing'} />);
 }
---
+++ blocks/testcomponent/testcomponent.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function MyReactComponent({ message }) {
+  return <div className="react-greeting">Hello from React! Message: {message}</div>;
+}
