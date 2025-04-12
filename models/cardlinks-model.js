5ab5a565f1f403d82ed9e7eff9c75df8284ba38b
Krzysztof Poznakowski <kpoznak@gmail.com>
2025-04-12 17:20:47 +0200

    cardlinks model

---
+++ models/cardlinks-model.js
@@ -0,0 +1,14 @@
+export default {
+    fields: {
+        title: {
+            type: 'string',
+            label: 'Title',
+            required: true
+        },
+        subtitle: {
+            type: 'string',
+            label: 'Subtitle',
+            required: true
+        }
+    }
+};
